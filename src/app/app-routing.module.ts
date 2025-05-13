3ffaf07e4204110a98f2be81074191e1d66e8600
aluno <@marcosqwo>
2025-05-13 16:43:54 -0300

    segunda alteracao do trabalho

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaginaInicialComponent } from './componentes/pagina-inicial/pagina-inicial.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',redirectTo:"pagina-inicial",pathMatch:"full"},
+  {path: 'pagina-inicial', component: PaginaInicialComponent}
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaginaInicialComponent } from './componentes/pagina-inicial/pagina-inicial.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaInicialComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
